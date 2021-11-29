64994b8339541c9256f8534f5575f18b17cd6a90
Mahlet291 <mahletkumelachew1@gmail.com>
2021-11-28 23:34:11 -0800

    Print uppercase letters

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for word in str:
+        if ord(word) >= 97 and ord(word) <= 127):
+            print("{}".format(chr(ord(word) - 32)), end="")
+        else:
+            print("{}".format(word), end="")
