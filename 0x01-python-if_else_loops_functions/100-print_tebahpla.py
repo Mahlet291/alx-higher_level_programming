f905ea0e7b9f50272b782d5eb315e3990a493e59
Mahlet291 <mahletkumelachew1@gmail.com>
2021-12-03 19:28:32 -0800

    print the alphabet

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(122, 96):
-    print("{:c}".format(chr(i - 32)) if i % 2 == 0 else "{:c}".format(chr(i)), end="") 
+for i in range(122, 96, -1):
+    print("{}".format(chr(i - 32)) if i % 2 == 0 else "{}".format(chr(i)), end="") 
     
